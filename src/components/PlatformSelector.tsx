021bd7107490f6605652d268aa34ecac5269ed0a
Natnael Getnet <NatnaelGetnetHaregewoyn@gmail.com>
2024-04-29 15:26:08 +0300

    Filtered games by platform

--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -1,8 +1,15 @@
-import usePlatform from "@/hooks/usePlatform";
+import usePlatform, { Platform } from "@/hooks/usePlatform";
 import { Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
 import { BsChevronDown } from "react-icons/bs";
 
-export const PlatformSelector = () => {
+interface Props {
+  onselectedPlatform: (platform: Platform) => void;
+  selectedPlatform: Platform | null;
+}
+export const PlatformSelector = ({
+  onselectedPlatform,
+  selectedPlatform,
+}: Props) => {
   const { data, error, isLoading } = usePlatform();
 
   if (error.length) return null;
@@ -11,11 +18,20 @@ export const PlatformSelector = () => {
   return (
     <Menu>
       <MenuButton as={Button} rightIcon={<BsChevronDown />}>
-        Platform
+        {selectedPlatform ? selectedPlatform.name : "Platforms"}
       </MenuButton>
       <MenuList>
         {data.map((platform) => {
-          return <MenuItem key={platform.id}>{platform.name}</MenuItem>;
+          return (
+            <MenuItem
+              onClick={() => {
+                onselectedPlatform(platform);
+              }}
+              key={platform.id}
+            >
+              {platform.name}
+            </MenuItem>
+          );
         })}
       </MenuList>
     </Menu>
