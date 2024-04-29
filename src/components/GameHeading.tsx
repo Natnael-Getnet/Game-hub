89402bd62e3d4bfaf072376da708625dd9c1f975
Natnael Getnet <NatnaelGetnetHaregewoyn@gmail.com>
2024-04-29 22:22:17 +0300

    Added a daynamic page haeder

---
+++ src/components/GameHeading.tsx
@@ -0,0 +1,17 @@
+import { GameQuery } from "@/App";
+import { Heading } from "@chakra-ui/react";
+
+interface Props {
+  gameQuery: GameQuery;
+}
+
+export const GameHeading = ({ gameQuery }: Props) => {
+  const heading = `${gameQuery.platform?.name || ""} ${
+    gameQuery.genre?.name || ""
+  } Games`;
+  return (
+    <Heading paddingY={5} as={"h1"} fontSize="5xl">
+      {heading}
+    </Heading>
+  );
+};
